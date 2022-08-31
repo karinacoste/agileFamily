beda838aa6289c13d3fd3693b9151c0748931123
kcoste <karinacoste@yahoo.es>
2022-08-31 14:28:35 +0200

    Added home page banner

--- src/components/banners/TextBanner.vue
+++ src/components/banners/TextBanner.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="w-full bg-blue-banner h-96 flex items-center justify-center">
-    <div class="w-5/6 flex">
-      <div class="text-4xl w-full md:w-1/2 text-white">
-        <div class="w-full md:w-4/6 ml-auto text-center md:text-right">
+  <div class="w-full bg-blue-banner flex items-center justify-center">
+    <div class="w-5/6 flex my-16 sm:my-0 md:my-10 lg:my-0 md:flex-row flex-col">
+      <div
+        class="flex md:flex-row flex-col text-4xl w-full md:w-1/2 text-white items-center"
+      >
+        <div class="w-full md:w-4/6 ml-auto text-center md:text-right md:mr-32">
           ¡Organiza y gestiona los proyectos y tareas de tu familia!
         </div>
       </div>
-      <div class="hidden md:w-1/2"></div>
+      <div class="flex w-full md:w-1/2">
+        <div class="md:w-full">
+          <img
+            src="../../assets/images/familyImage.png"
+            alt="agileFamily Logo"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
