0adf84ae38ed085c9b6cd65eccb03266e2bf59a7
kcoste <karinacoste@yahoo.es>
2022-10-06 13:49:14 +0200

    Added nested draggable for objectives drag

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="w-full h-screen bg-gray-100">
+  <div class="w-full h-full bg-gray-100">
     <!-- <p class="font-bold">showedUser {{ showedUser.email }}</p> -->
     <internal-nav-bar :user="userDisplayName"></internal-nav-bar>
     <h1 class="text-2xl font-bold ml-7 my-5">SPRINT 1 {{ userDisplayName }}</h1>
     <!-- <h1 class="text-5xl">{{ showedUser.email }}</h1> -->
     <!-- <button @click="addUser">Crear user</button>
     <button @click="showUsers">Show user</button> -->
-
     <div class="w-full flex justify-between p-2 m-auto">
       <div class="w-1/5 px-5">
         <div class="w-full py-2 font-bold border-b-4 border-gray-300">
@@ -48,100 +47,55 @@
     </div>
 
     <!-- // ////////////////////////////////// -->
-    <div class="w-full flex justify-between p-2">
-      <div class="w-1/5 px-4">
-        <div class="w-full h-48 px-2">1. Reformar el baño</div>
-      </div>
-      <!-- // ////// -->
-      <div class="w-1/5 px-5">
-        <draggable
-          :list="todos"
-          item-key="id"
-          class="w-full h-48"
-          group="my-group"
-        >
-          <template #item="{ element }">
-            <div
-              class="list-group-item bg-white shadow-md mb-6 px-6 py-4 hover:cursor-move"
-            >
-              <div class="font-semibold text-lg pb-12">
-                {{ element.id }} - {{ element.name }}
-              </div>
-              <div class="flex items-center pb-3 pr-4">
-                <img
-                  :src="require(`@/assets/images/users/${element.userImg}.png`)"
-                  alt="user image"
-                />
 
-                <span class="ml-2">{{ element.priority }}</span>
-                <span class="ml-2">{{ element.estimateTime }}</span>
-              </div>
-            </div>
-          </template>
-        </draggable>
-      </div>
-      <!-- // //// -->
-      <div class="w-1/5 px-5">
-        <draggable
-          :list="inProgress"
-          item-key="id"
-          class="w-full h-48"
-          group="my-group"
+    <draggable
+      :list="list"
+      item-key="id"
+      class="w-full h-full"
+      group="my-group"
+    >
+      <template #item="{ element }"
+        ><div
+          class="w-full flex justify-between p-2 border-b-4 pt-6 border-gray-200"
         >
-          <template #item="{ element }">
-            <div
-              class="list-group-item bg-white shadow-md mb-6 px-6 py-4 hover:cursor-move"
-            >
-              <div class="font-semibold text-lg pb-12">
-                {{ element.id }} - {{ element.name }}
-              </div>
-              <div class="flex items-center pb-3 pr-4">
-                <img
-                  :src="require(`@/assets/images/users/${element.userImg}.png`)"
-                  alt="user image"
-                />
+          <div class="w-1/5 px-4">
+            <div class="w-full h-48 px-2">{{ element.name }}</div>
+          </div>
+          <div v-for="state in element.states" :key="state" class="w-1/5 px-5">
+            <nested-draggable>
+              <draggable
+                :list="state.items"
+                item-key="id"
+                class="w-full h-full"
+                group="my-group2"
+              >
+                <template #item="{ element }"
+                  ><div
+                    class="list-group-item bg-white shadow-md mb-6 px-6 py-4 hover:cursor-move"
+                  >
+                    <div class="font-semibold text-lg pb-12">
+                      {{ element.id }} - {{ element.name }}
+                    </div>
+                    <div class="flex items-center pb-3 pr-4">
+                      <img
+                        :src="
+                          require(`@/assets/images/users/${element.userImg}.png`)
+                        "
+                        alt="user image"
+                      />
 
-                <span class="ml-2">{{ element.priority }}</span>
-                <span class="ml-2">{{ element.estimateTime }}</span>
-              </div>
-            </div>
-          </template>
-        </draggable>
-      </div>
-      <!-- //////// -->
-      <div class="w-1/5 px-5">
-        <draggable
-          :list="block"
-          item-key="id"
-          class="w-full h-48"
-          group="my-group"
-        >
-          <template #item="{ element }">
-            <div
-              class="list-group-item bg-white shadow-md mb-6 px-7 py-3 hover:cursor-move"
-            >
-              {{ element.name }}
-            </div>
-          </template>
-        </draggable>
-      </div>
-      <div class="w-1/5 px-5">
-        <draggable
-          :list="completed"
-          item-key="id"
-          class="w-full h-48"
-          group="my-group"
-        >
-          <template #item="{ element }">
-            <div
-              class="list-group-item bg-white shadow-md mb-6 px-7 py-3 hover:cursor-move"
-            >
-              {{ element.name }}
-            </div>
-          </template>
-        </draggable>
-      </div>
-    </div>
+                      <span class="ml-2">{{ element.priority }}</span>
+                      <span class="ml-2">{{ element.estimateTime }}</span>
+                    </div>
+                  </div>
+                </template>
+              </draggable>
+            </nested-draggable>
+          </div>
+        </div>
+      </template>
+    </draggable>
+
     <!-- <div class="w-full px-7">
       <div class="h-1 bg-gray-300"></div>
     </div> -->
@@ -157,6 +111,7 @@ import { useStore } from 'vuex'
 import InternalNavBar from '@/components/nav/InternalNavBar.vue'
 import { onBeforeMount } from 'vue'
 import draggable from 'vuedraggable'
+import mock from '@/assets/mock2.json'
 // import NavBar from '../components/nav/NavBar.vue'
 
 export default {
@@ -179,7 +134,7 @@ export default {
       }
     }
     console.log('user', store.state.user)
-
+    const list = ref(mock)
     const inProgress = ref([
       {
         id: 1,
@@ -232,6 +187,7 @@ export default {
       inProgress,
       completed,
       block,
+      list,
     }
   },
 }
